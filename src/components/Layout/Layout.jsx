ec7390319d0df8fa36dab8cb36e4d20072dc629c
urjiiko1 <gemachistesfaye36@gmail.com>
2026-06-02 14:16:56 +0300

    feat: create Layout component with sidebar integration and responsive content spacing

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Sidebar } from "../Sidebar/Sidebar";
 
 export const Layout = ({ children, isDarkMode = false }) => {
   return (
-    <div className={`min-h-screen ${isDarkMode ? "dark" : ""} bg-transparent`}>
+    <div className={`min-h-screen ${isDarkMode ? "dark" : ""} bg-transparent flex relative`}>
       
       {/* Sidebar */}
       <Sidebar />
